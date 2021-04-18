34614c33e4fc07a3ca31d54b45379402b9b57a39
Gaurav Kumar <gauravk26800@gmail.com>
2021-04-18 19:00:54 +0530

    fixed styling of education section

--- src/components/Education.js
+++ src/components/Education.js
@@ -2,72 +2,78 @@ import React from "react";
 
 const Education = () => {
   return (
-    <div class="education p-5">
-      <h1 class="education-child heading">Formal Education</h1>
-      <div class="education-child holder">
-        <div class="holder-child education-card-1">
-          <div class="edu-class-head">
-            <h2>B. Tech.</h2>
-          </div>
-          <div class="edu-class-body">
-            <a href="http://mnnit.ac.in" target="_blank" rel="noreferrer">
-              <img
-                class="edu-logo"
-                src="/assets/images/mnnit.png"
-                alt="MNNIT icon"
-              />
-            </a>
-            <h3>Motilal Nehru National Institute of Technology Allahabad</h3>
-            <h4>
-              <strong>Electronics &amp; Communication :</strong> 7.25 (Currently
-              in 6th Semester)
-            </h4>
-          </div>
-        </div>
-
-        <div class="holder-child education-card-1">
-          <div class="edu-class-head">
-            <h2>Class XII</h2>
-          </div>
-          <div class="edu-class-body">
-            <a
-              href="https://www.navodaya.gov.in/nvs/nvs-school/SONBHADRA/en/home/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img
-                class="edu-logo"
-                src="/assets/images/jnv.jpeg"
-                alt="JNV Icon"
-              />
-            </a>
-            <h3>Jawahar Navodaya Vidyalaya Sonbhadra</h3>
-            <h4>
-              <strong>CBSE :</strong> 92.8%
-            </h4>
+    <div className="education p-5">
+      <h1 className="education-child heading">Formal Education</h1>
+      <div className="holder">
+        <div className="holder-child education-card-1">
+          <h2 className="edu-class-head">B. Tech.</h2>
+          <div className="edu-class-body">
+            <div className="edu-icon">
+              <a href="http://mnnit.ac.in" target="_blank" rel="noreferrer">
+                <img
+                  className="edu-logo"
+                  src="/assets/images/mnnit.png"
+                  alt="MNNIT icon"
+                />
+              </a>
+            </div>
+            <div className="edu-data">
+              <h3>Motilal Nehru National Institute of Technology Allahabad</h3>
+              <h4>
+                <strong>Electronics &amp; Communication :</strong> 7.25
+                (Currently in 6th Semester)
+              </h4>
+            </div>
           </div>
         </div>
 
-        <div class="holder-child education-card-1">
-          <div class="edu-class-head">
-            <h2>Class X</h2>
+        <div className="holder-child education-card-2">
+          <h2 className="edu-class-head">Class XII</h2>
+          <div className="edu-class-body">
+            <div className="edu-icon">
+              <a
+                href="https://www.navodaya.gov.in/nvs/nvs-school/SONBHADRA/en/home/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  className="edu-logo"
+                  src="/assets/images/jnv.jpeg"
+                  alt="JNV Icon"
+                />
+              </a>
+            </div>
+            <div className="edu-data">
+              <h3>Jawahar Navodaya Vidyalaya Sonbhadra</h3>
+              <h4>
+                <strong>CBSE :</strong> 92.8%
+              </h4>
+            </div>
           </div>
-          <div class="edu-class-body">
-            <a
-              href="https://www.navodaya.gov.in/nvs/nvs-school/SONBHADRA/en/home/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img
-                class="edu-logo"
-                src="/assets/images/jnv.jpeg"
-                alt="JNV Icon"
-              />
-            </a>
-            <h3>Jawahar Navodaya Vidyalaya Sonbhadra</h3>
-            <h4>
-              <strong>CBSE :</strong> 91.2%
-            </h4>
+        </div>
+
+        <div className="holder-child education-card-3">
+          <h2 className="edu-class-head">Class X</h2>
+          <div className="edu-class-body">
+            <div className="edu-icon">
+              <a
+                href="https://www.navodaya.gov.in/nvs/nvs-school/SONBHADRA/en/home/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  className="edu-logo"
+                  src="/assets/images/jnv.jpeg"
+                  alt="JNV Icon"
+                />
+              </a>
+            </div>
+            <div className="edu-data">
+              <h3>Jawahar Navodaya Vidyalaya Sonbhadra</h3>
+              <h4>
+                <strong>CBSE :</strong> 91.2%
+              </h4>
+            </div>
           </div>
         </div>
       </div>
