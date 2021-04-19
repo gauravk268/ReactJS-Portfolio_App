4f1e390635ec34814e4e93acd54975003211e602
Gaurav Kumar <gauravk26800@gmail.com>
2021-04-19 23:39:34 +0530

    fixed styling of project card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,9 +3,9 @@ import React from "react";
 function ProjectCard(props) {
   return (
     <div className="projectCard p-2">
-      <h1>{props.project.title}</h1>
-      <h3>{props.project.description}</h3>
-      <button class="btn btn-link">
+      <h1 className="project-card title">{props.project.title}</h1>
+      <h3 className="project-card desc">{props.project.description}</h3>
+      <button class="btn btn-primary">
         <a href={props.project.githubLink}>See more</a>
       </button>
     </div>
