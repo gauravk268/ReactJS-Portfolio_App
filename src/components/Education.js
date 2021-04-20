897fc9d200fd6b281073f531c0718a125004f5bc
Gaurav Kumar <gauravk26800@gmail.com>
2021-04-21 00:28:04 +0530

    changed education layout to card

--- src/components/Education.js
+++ src/components/Education.js
@@ -2,77 +2,77 @@ import React from "react";
 
 const Education = () => {
   return (
-    <div className="education p-5">
+    <div className="section-education">
       <h1 className="education-child heading">Formal Education</h1>
-      <div className="holder">
-        <div className="holder-child education-card-1">
-          <h2 className="edu-class-head">B. Tech.</h2>
-          <div className="edu-class-body">
-            <div className="edu-icon">
-              <a href="http://mnnit.ac.in" target="_blank" rel="noreferrer">
-                <img
-                  className="edu-logo"
-                  src="/assets/images/mnnit.png"
-                  alt="MNNIT icon"
-                />
-              </a>
-            </div>
-            <div className="edu-data">
-              <h3>Motilal Nehru National Institute of Technology Allahabad</h3>
-              <h4>
+      <div className="holder row">
+        <div className="project-card col-lg-4 col-md-6 col-12 mb-5 d-flex">
+          <div className="card flex-fill">
+            <a href="https://www.mnnit.ac.in" target="_blank" rel="noreferrer">
+              <img
+                className="card-img-top p-4"
+                src="/assets/images/mnnit.png"
+                alt="Card cap"
+              />
+            </a>
+            <div className="card-body">
+              <h3 className="card-title">B. Tech.</h3>
+              <h4 className="project-card card-title">
+                Motilal Nehru National Institute of Technology Allahabad
+              </h4>
+              <p className="project-card card-text">
                 <strong>Electronics &amp; Communication :</strong> 7.25
                 (Currently in 6th Semester)
-              </h4>
+              </p>
             </div>
           </div>
         </div>
 
-        <div className="holder-child education-card-2">
-          <h2 className="edu-class-head">Class XII</h2>
-          <div className="edu-class-body">
-            <div className="edu-icon">
-              <a
-                href="https://www.navodaya.gov.in/nvs/nvs-school/SONBHADRA/en/home/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  className="edu-logo"
-                  src="/assets/images/jnv.jpeg"
-                  alt="JNV Icon"
-                />
-              </a>
-            </div>
-            <div className="edu-data">
-              <h3>Jawahar Navodaya Vidyalaya Sonbhadra</h3>
-              <h4>
+        <div className="project-card col-lg-4 col-md-6 col-12 mb-5 d-flex">
+          <div className="card flex-fill">
+            <a
+              href="https://www.navodaya.gov.in/nvs/nvs-school/SONBHADRA/en/home/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="card-img-top p-4"
+                src="/assets/images/jnv.jpeg"
+                alt="Card cap"
+              />
+            </a>
+            <div className="card-body">
+              <h3 className="card-title">Class XII</h3>
+              <h4 className="project-card card-title">
+                Jawahar Navodaya Vidyalaya Sonbhadra
+              </h4>
+              <p className="project-card card-text">
                 <strong>CBSE :</strong> 92.8%
-              </h4>
+              </p>
             </div>
           </div>
         </div>
 
-        <div className="holder-child education-card-3">
-          <h2 className="edu-class-head">Class X</h2>
-          <div className="edu-class-body">
-            <div className="edu-icon">
-              <a
-                href="https://www.navodaya.gov.in/nvs/nvs-school/SONBHADRA/en/home/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  className="edu-logo"
-                  src="/assets/images/jnv.jpeg"
-                  alt="JNV Icon"
-                />
-              </a>
-            </div>
-            <div className="edu-data">
-              <h3>Jawahar Navodaya Vidyalaya Sonbhadra</h3>
-              <h4>
-                <strong>CBSE :</strong> 91.2%
+        <div className="project-card col-lg-4 col-md-6 col-12 mb-5 d-flex">
+          <div className="card flex-fill">
+            <a
+              href="https://www.navodaya.gov.in/nvs/nvs-school/SONBHADRA/en/home/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="card-img-top p-4"
+                src="/assets/images/jnv.jpeg"
+                alt="Card cap"
+              />
+            </a>
+            <div className="card-body">
+              <h3 className="card-title">Class X</h3>
+              <h4 className="project-card card-title">
+                Jawahar Navodaya Vidyalaya Sonbhadra
               </h4>
+              <p className="project-card card-text">
+                <strong>CBSE :</strong> 91.2%
+              </p>
             </div>
           </div>
         </div>
