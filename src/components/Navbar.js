49d594936a61f56f1e645d66e17dae1d7e26e5a9
Gaurav Kumar <gauravk26800@gmail.com>
2021-04-30 21:54:50 +0530

    added smooth scroll in html remove react-scroll package

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,40 @@
-import { Link, animateScroll as scroll } from "react-scroll";
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 
 const Navbar = () => {
-  const scrollToTop = () => {
-    scroll.scrollToTop({ smooth: "easeInOutCubic", });
-  };
-
   return (
     <div className="navbar">
       <div className="navbar-child navbar-left">
-        <div className="navbar-brand">
+        <div className="navbar-brand" id="brand">
           <a href="/">Gaurav</a>
         </div>
       </div>
       <div className="navbar-child navbar-right">
+        {useLocation().pathname === "/" ? (
+          <div>
+            <a className="navbar-right-child navbar-item" href="#projects">
+              Projects
+            </a>
+          </div>
+        ) : (
+          <a className="navbar-right-child navbar-item" href="/#projects"></a>
+        )}
 
-
-        {(useLocation().pathname === "/") ? <Link className="navbar-right-child navbar-item" activeClass="active" to="projects" smooth={true} duration={700}>
-          Projects</Link> : <a className="navbar-right-child navbar-item"  href="/#projects">Projects</a>}
-
-        {(useLocation().pathname === "/") ? <Link className="navbar-right-child navbar-item" activeClass="active" to="achiev" smooth={true} duration={700}>
-          Achievements
-        </Link> : <a className="navbar-right-child navbar-item" href="/#achiev">Achievements</a>}
+        {useLocation().pathname === "/" ? (
+          <a className="navbar-right-child navbar-item" href="#achiev">
+            Achievements
+          </a>
+        ) : (
+          <a className="navbar-right-child navbar-item" href="/#achiev"></a>
+        )}
 
         <div className="navbar-right-child navbar-item">
           <a href="/contact">Contact</a>
         </div>
-
       </div>
 
-      <button onClick={scrollToTop} className="btn top btn-primary" >
+      <a className="btn top btn-primary" href="#brand">
         &#8682;
-      </button>
-
-
+      </a>
     </div>
   );
 };
