6337871116a61b221af44fb23c954c1d71cc1ff9
Gaurav Kumar <gauravk26800@gmail.com>
2021-05-02 16:50:13 +0530

    sending message service online added firebase to save message

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,21 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import db from "./Firebase/config";
 
 const Feedback = () => {
+  const [feedTemp, setFeedTemp] = useState({ name: "", email: "", msg: "" });
+  const [saving, setSaving] = useState(false);
+  const [sent, setSent] = useState(false);
+  const [error, setError] = useState();
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (feedTemp.name === "" || feedTemp.email === "" || feedTemp.msg === "")
+      return;
+
+    setSaving(true);
+    console.log(feedTemp);
+
+    db.collection("feedback")
+      .add(feedTemp)
+      .then((documentReference) => {
+        setSent(true);
+        setError(false);
+        console.log("document reference ID", documentReference.id);
+      })
+      .catch((error) => {
+        setError(true);
+        setSent(true);
+        console.log(error.message);
+      });
+
+    setFeedTemp({ name: "", email: "", msg: "" });
+    setSaving(false);
   };
+
   return (
     <div className="section-feedback p-5" id="about">
       <h1 className="feedback heading">Get in touch</h1>
-      <small className="alert-danger">
-        Service currently not working.
-        <br /> Mail on{" "}
-        <a href="mailto:gauravk26800@gmail.com">gauravk26800@gmail.com</a>
-        <br /> Sorry for the inconvenience.
-      </small>
+      <div className="status">
+        {sent &&
+          (error === false ? (
+            <small className="alert-success">Message saved successfully.</small>
+          ) : (
+            <small className="alert-danger">
+              Cannot save message.
+              <br /> Mail on{" "}
+              <a href="mailto:gauravk26800@gmail.com">
+                gauravk26800@gmail.com
+              </a>{" "}
+              or try again later.
+              <br /> Sorry for the inconvenience.
+            </small>
+          ))}
+      </div>
       <hr />
       <div className="feedback-wrapper">
-        <form action="https://gauravk268.herokuapp.com/feedback" method="post">
+        <form>
           <div className="feedback-element feedback-head">
             <div className="form-group">
               <label for="name">
@@ -27,6 +66,10 @@ const Feedback = () => {
                 name="name"
                 placeholder="Enter name"
                 required=""
+                value={feedTemp.name}
+                onChange={(e) => {
+                  setFeedTemp({ ...feedTemp, name: e.target.value });
+                }}
               />
             </div>
             <div className="form-group">
@@ -40,6 +83,10 @@ const Feedback = () => {
                 aria-describedby="emailHelp"
                 placeholder="Enter email"
                 required=""
+                value={feedTemp.email}
+                onChange={(e) => {
+                  setFeedTemp({ ...feedTemp, email: e.target.value });
+                }}
               />
             </div>
           </div>
@@ -49,11 +96,20 @@ const Feedback = () => {
               <label for="feedback">
                 <h3>Message</h3>
               </label>
-              <textarea className="form-control" name="feedback"></textarea>
+              <textarea
+                className="form-control"
+                name="feedback"
+                value={feedTemp.msg}
+                placeholder="Your message here"
+                onChange={(e) => {
+                  setFeedTemp({ ...feedTemp, msg: e.target.value });
+                }}
+              ></textarea>
             </div>
             <button
               onClick={handleSubmit}
               className="btn btn-primary my-2 w-100"
+              disabled={saving}
             >
               Send
             </button>
