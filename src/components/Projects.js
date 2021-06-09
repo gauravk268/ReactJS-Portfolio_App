283d418a7325457eaaa8cd2feda9ec6794770464
Gaurav Kumar <gauravk26800@gmail.com>
2021-06-09 14:52:57 +0530

    UPDATE: added carousel for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,10 @@ import React from "react";
 import { useData } from "./contexts";
 import ProjectCard from "./ProjectCard";
 
+import makeCarousel from "react-reveal/makeCarousel";
+import Slide from "react-reveal/Slide";
+import "./Project.css";
+
 function Projects(props) {
   const { data } = useData();
 
@@ -11,12 +15,51 @@ function Projects(props) {
         <h1 className="display-6">Projects</h1>
         <hr />
         <div className="row">
+          
           {data.projects.map((project) => {
             return <ProjectCard project={project} key={project.key} />;
           })}
+
+          <Slider projects={data.projects} />
         </div>
       </div>
     </section>
+  );
+}
+
+function Slider({ projects }) {
+  const CarouselUI = ({ position, total, handleClick, children }) => (
+    <div className="Conatainer">
+      <div className="Children">
+        {children}
+        <div className="Arrow Left" onClick={handleClick} data-position={position - 1}>
+          {"<"}
+        </div>
+        <div className="Arrow Right" right onClick={handleClick} data-position={position + 1}>
+          {">"}
+        </div>
+      </div>
+      <div className="Dots">
+        {Array(...Array(total)).map((val, index) => (
+          <div className="Dot" key={index} onClick={handleClick} data-position={index}>
+            {index === position ? "● " : "○ "}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+  const Carousel = makeCarousel(CarouselUI);
+
+  return (
+    <Carousel>
+      {projects.map((project) => {
+        return (
+          <Slide right>
+            <ProjectCard project={project} key={project.key} />
+          </Slide>
+        );
+      })}
+    </Carousel>
   );
 }
 
