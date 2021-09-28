4415a1942199b79ab64a1fb7e5a4f8977fb6af89
Gaurav Kumar <gauravk26800@gmail.com>
2021-09-29 00:57:53 +0530

    FIX: removed unused component import

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import About from "./About";
-import Education from "./Education";
+// import Education from "./Education";
 import Skills from "./Skills";
 import Projects from "./Projects";
 import Courses from "./Courses";
