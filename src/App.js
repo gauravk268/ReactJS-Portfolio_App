9d16dbffacf43c83bf1bc633c6adbd5f6a91aae4
gaurav <gauravk26800@gmail.com>
2025-03-13 00:47:15 +0530

    initialied a new react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { DataProvider } from "./components/contexts";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Homepage, ReadMsg, Footer, Pictures } from "./components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataProvider>
+      <Router>
+        <div className="App">
+          <Navbar />
+
+          <Switch>
+            <Route path="/" exact component={() => <Homepage />} />
+            <Route path="/msg" exact component={() => <ReadMsg />} />
+            <Route path="/pics" exact component={() => <Pictures />} />
+            <Route path="*" component={() => <Homepage />} />
+          </Switch>
+
+          <Footer />
+        </div>
+      </Router>
+    </DataProvider>
   );
 }
 
